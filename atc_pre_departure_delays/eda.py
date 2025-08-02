a4a00338990dfc076f86ae5a053f3c5089970efd
Patryk <ostrowski.patryk@gmail.com>
2025-08-02 22:30:41 +0200

    Polecenia EDA na dataframe

--- atc_pre_departure_delays/eda.py
+++ atc_pre_departure_delays/eda.py
@@ -14,3 +14,44 @@ print(f'Dataset path is {dataset}')
 df = pd.read_csv(dataset)
 print('Dataframe:')
 print(df)
+print()
+
+
+##############################################
+##########    eksploracja zbioru    ##########
+##############################################
+
+print('Pierwsze 5 wierszy:')
+print(df.head())
+print()
+
+print('Ostatnie 5 wierszy:')
+print(df.tail())
+print()
+
+print('Pierwsze 9 wierszy:')
+print(df.head(9))
+print()
+
+print('Ostatnie 11 wierszy:')
+print(df.tail(11))
+print()
+
+print('Losowe 5 wierszy:')
+print(df.sample())
+print()
+
+print('Losowe 7 wierszy:')
+print(df.sample(7))
+print()
+
+print('Informacje o kolumnach:')
+print(df.info())
+print()
+
+print('Opis data frame:')
+print(df.describe())
+print()
+
+print('Unikaty w kolumnach:')
+print(df.nunique())
